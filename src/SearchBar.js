96a2b1dd63ef79e42ed33d31a2b773ab4bee5e4a
AAron Staton <Orson108@hotmail.com>
2020-08-11 15:27:44 -0400

    Add - Search bar for the app

---
+++ src/SearchBar.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  searchOnEnter({ keyCode }) {
+    if (keyCode === 13) {
+      this.clickHandler();
+    }
+  }
+
+  componentDidMount() {
+    const searchInputElem = document.getElementById("searchInput");
+    searchInputElem.addEventListener("keyup", (evt) => this.searchOnEnter(evt));
+  }
+
+  componentWillUnmount() {
+    const searchInputElem = document.getElementById("searchInput");
+    searchInputElem.removeEventListener("keyup", (evt) =>
+      this.searchOnEnter(evt)
+    );
+  }
+
+  clickHandler() {
+    const searchInputElem = document.getElementById("searchInput");
+    const breed = searchInputElem.value;
+    if (breed) {
+      searchInputElem.value = "";
+      this.props.searchForBreed(breed);
+    }
+  }
+
+  render() {
+    return (
+      <div className="searchWrapper">
+        <label htmlFor="searchInput">
+          Enter the breed of dog to search for
+        </label>
+        <br></br>
+        <input
+          type="text"
+          id="searchInput"
+          name="searchInput"
+          defaultValue="poodle"
+        />
+        <button onClick={() => this.clickHandler()}>Search</button>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
