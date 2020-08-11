c5500293de11794d1064d725a3dd9aab8bb78a85
AAron Staton <Orson108@hotmail.com>
2020-08-11 16:14:17 -0400

    add - build folder files

---
+++ build/precache-manifest.5f3dcd09b8b2f64a05aa58cad6a86f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69393df18c391bd269b12bf8841f37fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5aeec943ea4af23f61c",
+    "url": "/static/css/main.55d11f28.chunk.css"
+  },
+  {
+    "revision": "363d0bd99ee76a098771",
+    "url": "/static/js/2.13348848.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13348848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5aeec943ea4af23f61c",
+    "url": "/static/js/main.20e026f4.chunk.js"
+  },
+  {
+    "revision": "4e7efd94cd16dbfdad6f",
+    "url": "/static/js/runtime-main.19a00d09.js"
+  }
+]);
